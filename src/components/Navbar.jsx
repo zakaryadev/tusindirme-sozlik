da59863a08ab9210487bf1e489d0674a883d48bf
zakaryadev <zakaryamuratbaev13@gmail.com>
2023-01-14 11:27:13 +0500

    active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,39 +3,52 @@ import logo from "../assets/img/logo.svg";
 import words from "../assets/icons/words.svg";
 import wordsList from "../assets/icons/wordslist.svg";
 import infoIcon from "../assets/icons/info.svg";
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import Footer from "./Footer";
 
 function Navbar() {
+  let activeStyle = {
+    opacity: 1,
+  };
+
   return (
     <React.Fragment>
       <header>
         <div className="container">
           <nav className="navbar">
-            <Link to="/home">
+            <NavLink to="/home">
               <span className="navbar-logo">
                 <img src={logo} alt="Logo" />
               </span>
-            </Link>
+            </NavLink>
 
             <ul className="navbar-links">
               <li className="navbar-links_link">
-                <Link to="/words">
+                <NavLink
+                  to="/words"
+                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                >
                   <img src={words} alt="words" />
                   Sózler
-                </Link>
+                </NavLink>
               </li>
               <li className="navbar-links_link">
-                <Link to="/wordslist">
+                <NavLink
+                  to="/wordslist"
+                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                >
                   <img src={wordsList} alt="wordsList" />
                   Sózler Dizimi
-                </Link>
+                </NavLink>
               </li>
               <li className="navbar-links_link">
-                <Link to="/about">
+                <NavLink
+                  to="/about"
+                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                >
                   <img src={infoIcon} alt="infoIcon" />
                   Baǵdarlama haqqında
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </nav>
