2fa3f23ecadf73f04adbc9c069482aa99a9ca1b7
zakaryadev <zakaryamuratbaev13@gmail.com>
2023-01-15 23:14:11 +0500

    about as

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 export const About = () => {
-  return <div>About page</div>;
-}
+  return (
+    <section className="about">
+      <div className="container">
+        <h2 className="about-title">Biz haqqımızda</h2>
+        <p className="about-desc">
+          Quis enim tristique tempor et maecenas neque consectetur. Ac ut sed
+          egestas dictum nisi. Leo molestie cursus id varius massa augue. Erat
+          tincidunt venenatis aliquet nulla. Vitae curabitur nunc risus egestas
+          sed egestas eget egestas. Etiam faucibus elementum eu suspendisse.
+          Convallis vel purus viverra turpis massa quis pretium est. Pretium mi
+          consectetur tellus ultricies pellentesque tortor. In pretium est
+          gravida vitae lacinia. Ut malesuada pulvinar amet ut. Nullam morbi sit
+          erat dictum sit quisque curabitur massa elementum. Semper auctor
+          sagittis sed vulputate sit tellus. Egestas fringilla mi et id.
+          Habitant eget enim eleifend ipsum elit diam proin. Cursus sagittis in
+          dui platea mi rutrum ipsum. Tempor in aliquet vitae pellentesque
+          aenean ultricies hac ut. Congue auctor urna sit quisque. Ut elit nulla
+          semper purus phasellus tellus in faucibus. Sed id sed erat lectus eu
+          nulla semper. Risus nulla imperdiet ultricies eget vel cras. Nibh
+          pellentesque scelerisque aenean.
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default About;
