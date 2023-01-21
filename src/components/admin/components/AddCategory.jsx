2b60b1342d6e76dc4021c8f928adf267caa10d15
zakaryadev <zakaryamuratbaev13@gmail.com>
2023-01-21 17:52:00 +0500

    add category form

---
+++ src/components/admin/components/AddCategory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddCategory = () => {
+  return (
+    <div className="add-category">
+      <input type="text" placeholder="Kategoriya" />
+      <button className="submit-btn">Kiritiw</button>
+    </div>
+  );
+};
+
+export default AddCategory;
--- src/pages/AdminPage/Categories.jsx
+++ src/pages/AdminPage/Categories.jsx
@@ -1,7 +1,14 @@
+import React from "react";
+import AddCategory from "../../components/admin/components/AddCategory";
 import CategoriesList from "../../components/admin/components/CategoriesList";
 
 const Categories = () => {
-  return <CategoriesList />;
+  return (
+    <React.Fragment>
+      <CategoriesList />
+      <AddCategory />
+    </React.Fragment>
+  );
 };
 
 export default Categories;
